0b4465bfed9d22ee88dc2f45e129a6fd025668b5
cr0ss0vr <merryck_wales@hotmail.co.uk>
2024-07-01 23:57:57 +0100

    add sq;

---
+++ SQL scripts/001 - DefaultAnimals.sql
@@ -0,0 +1,10 @@
+USE [booking]
+GO
+
+INSERT INTO [dbo].[AnimalType]
+           ([Name],
+           [AppointmentLength])
+     VALUES
+           ('Dog', 60),
+		   ('Cat', 45),
+		   ('Rabbit', 30);
